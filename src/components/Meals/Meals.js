f299eaa1a2a43f8ddc3b289ab12f30076ff59055
NivedHari <nivedhari44@gmail.com>
2023-12-24 20:39:16 +0530

    Making the Items visible

--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,9 +1,11 @@
 import MealsSummary from "./MealsSummary";
+import AvailableMeals from "./AvailableMeals";
 import { Fragment } from "react";
 
 const Meals = () => {
     return <Fragment>
         <MealsSummary/>
+        <AvailableMeals/>
     </Fragment>
 }
 
