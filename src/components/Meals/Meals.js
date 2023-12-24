1f6847ca44fcb8b8d071ed20f1c002ed46eb2cb5
NivedHari <nivedhari44@gmail.com>
2023-12-24 20:15:02 +0530

    Restaurant Summary

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,10 @@
+import MealsSummary from "./MealsSummary";
+import { Fragment } from "react";
+
+const Meals = () => {
+    return <Fragment>
+        <MealsSummary/>
+    </Fragment>
+}
+
+export default Meals;
