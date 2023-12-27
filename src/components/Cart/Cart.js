267456d14ab7125187ceb6ba98d5df2a20157c37
NivedHari <nivedhari44@gmail.com>
2023-12-27 17:37:32 +0530

    Cart open and close

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,18 +1,18 @@
 import classes from './Cart.module.css';
 import Modal from '../UI/Modal';
 
-const Cart = () => {
+const Cart = (props) => {
 
     const cartItems = <ul className={classes['cart-items']}>{[{id: 'c1', name: 'Sushi', amount:2, price: 13.45}].map(item => <li>{item.name}</li>)}</ul>;
 
-    return <Modal>
+    return <Modal onClose={props.onClose}>
         {cartItems}
         <div className={classes.total}>
             <span >Total Amount</span>
             <span>35.52</span>
         </div>
         <div className={classes.actions}>
-            <button className={classes['button-alt']}>Close</button>
+            <button className={classes['button-alt']} onClick={props.onClose}>Close</button>
             <button className={classes.button}>Order</button>
         </div>
     </Modal>
